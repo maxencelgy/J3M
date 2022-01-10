e91e6ca5f9b262d3294f41b19807f0a633aa4b56
mathieu bey <matbey27110@gmail.com>
2022-01-10 15:14:30 +0100

    acueil

--- index.php
+++ index.php
@@ -1,7 +1,18 @@
 <?php
-include('inc/header_b.php');
-?>
-
-
+include('inc/header_b.php'); ?>
+<section id="acceuil">
+    <div class="wrap">
+        <div class="text">
+            <h2>Bienvenu sur notre site</h2>
+            <p>Bonjours, bienvenue sur notre site web, qui vous permettras d’analyse les trames réseaux de vos clients afin de détecter d’éventuelles attaque DdoS.
+                Grâce a notre site vous pourrais accéder à vos information réseaux avec des graphique simples a lire et a utilisé selon vos résultats.
+                Plusieurs graphiques seront proposés pour un aspect plus moderne.
+            </p>
+        </div>
+        <div>
+            <img src="asset/img/center-server-technology-network.jpg" alt="">
+        </div>
+    </div>
+</section>
 
 <?php include('inc/footer_b.php');

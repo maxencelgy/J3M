95989e7777bf4c19b006604dfbe2caab87befc61
Pinto Jody <pintojody20@gmail.com>
2022-01-10 14:44:00 +0100

    header

---
+++ inc/header_b.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="asset/css/styles.css">
+    <title>J3M</title>
+</head>
+<body>
+
+<header id="header">
+    <nav id="navigation">
+        <div class="logo_nav">
+            <img src="asset/img/logo.svg" alt="logo du site">
+        </div>
+
+        <div class="header_btn">
+            <a href="">Connexion</a>
+        </div>
+    </nav>
+</header>
--- index.php
+++ index.php
@@ -1,15 +1,7 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
-    <link rel="stylesheet" href="asset/sass/style.scss"> 
-    <title>J3M</title>
-</head>
-<body>
-    <h1>BOnjour Jody</h1>
-    <h2>Salut Maxence</h2>
-</body>
-</html>
+<?php
+include('inc/header_b.php');
+?>
+
+
+
+<?php include('inc/footer_b.php');

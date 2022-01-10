7cc70d85c5d01597d87e37dacaaabf30ae783133
mathieu bey <matbey27110@gmail.com>
2022-01-10 16:52:30 +0100

    modif index :sparkles:

--- index.php
+++ index.php
@@ -7,6 +7,9 @@ include('inc/header.php');?>
 
 <section id="acceuil">
     <div class="wrap">
+        <div class="img">
+            <img src="asset/img/network.jpg" alt="">
+        </div>
         <div class="text">
             <h2>Bienvenu sur notre site</h2>
             <p>Bonjours, bienvenue sur notre site web, qui vous permettras d’analyse les trames réseaux de vos clients afin de détecter d’éventuelles attaque DdoS.
@@ -14,9 +17,7 @@ include('inc/header.php');?>
                 Plusieurs graphiques seront proposés pour un aspect plus moderne.
             </p>
         </div>
-        <div>
-            <img src="asset/img/network.jpg" alt="">
-    </div>
+
 </section>
 
 

5e4e65cd44ec98aed1a0f7328dd0696850ecd912
maxencelgy <maxencelgy@gmail.com>
2022-01-10 16:07:58 +0100

    rangement sass

--- connexion.php
+++ connexion.php
@@ -8,13 +8,12 @@ include('inc/header.php');
         <h2>Connexion</h2>  
                 <label for="login">E-mail</label>
                 <input type="text" id="login" name="login" value="" placeholder="Entrez votre email">
-                <span class="error"><?= returnError($error,'login')?></span>
-
+               
                 <label for="password">Mot de passe *</label>
-                <input type="password" id="password" name="password" value="<?= returnValue('password') ?>" class="input" placeholder="Entrez votre mot de passe">
-                <span class="error"><?= returnError($error,'password')?></span>
-                <input type="submit" name="submitted" value="Connexion" class="submit input2">
-                <a href="lost-pwd.php">Mot de passe oublié ?</a>
+                <input type="password" id="password" name="password" value="" placeholder="Entrez votre mot de passe">
+                
+                <input type="submit" name="submitted" value="Connexion">
+                <a href="">Mot de passe oublié ?</a>
                 <p class="inscription">Si vous n'avez pas de compte <a href="inscription.php">inscrivez vous-ici</a></p>
             </form>
 </section>

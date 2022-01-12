f0377f69f9874be29a380668b7eeed59ec80b03c
mathieu bey <matbey27110@gmail.com>
2022-01-12 15:42:17 +0100

    pull

--- donnée.php
+++ donnée.php
@@ -1,12 +1,13 @@
 <?php
+session_start();
 require_once "config.php";
-// require_once "inc/fonction/pdo.php";
-// require_once "inc/fonction/toolbox.php";
+require_once "inc/fonction/pdo.php";
+require_once "inc/fonction/toolbox.php";
 
 include_once('inc/header.php');?>
 <section id="myChart">
     <div>
-
+        <canvas id="myChart"></canvas>
     </div>
 </section>
 

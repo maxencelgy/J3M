3ab1d05df5920722570e818501262a32de0e2459
MaxenceBossin <maxbossin25@gmail.com>
2022-01-13 17:19:30 +0100

    fin de journée

--- asset/js/ajax.js
+++ asset/js/ajax.js
@@ -1,7 +1,14 @@
 // Partie Ajax
-fetch('http://localhost/J3M/ajax/getDataJson.php')
-    .then (function(response){
-        return response.json()
-    }).then(function (data){
-    console.log(data)
-})
+const getDataJson = async function(){
+    let response = await fetch('http://localhost/J3M/ajax/getDataJson.php')
+    if (response.ok){
+        let data = await response.json()
+        return data
+    }else{
+      console.log('Erreur serveur : ', response.status)  
+    }
+    
+}
+console.log(getDataJson())
+
+

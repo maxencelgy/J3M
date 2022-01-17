2b02a818f66869c976aaf2acc2cf996e907a86db
maxencelgy <maxencelgy@gmail.com>
2022-01-17 09:33:43 +0100

    click tr ouvrir popup

--- asset/js/log.js
+++ asset/js/log.js
@@ -18,14 +18,14 @@ fetch('http://localhost/J3M/ajax/getDataJson.php')
             form.innerHTML += `
             <tr class="trame">
             <tr class="tra">
-                <td data-th="id">${element.identification}</td>
-                <td data-th="date">${date}</td>  
-                <td data-th="status">${element.status}</td>
-                <td data-th="name" class="elementName">${element.protocol_name}</td>
-                <td data-th="checksum" class="${protocolChecksumStatusClass}">${element.protocol_checksum__status}</td>
-                <td data-th="ttl" class="${ttlClass}">${element.ttl}</td>            
-                <td data-th="ipFrom">${ipFrom}</td>                 
-                <td data-th="ipDest">${ipDest}</td>                           
+                <td data-th="id" class="selectTd">${element.identification}</td>
+                <td data-th="date" class="selectTd">${date}</td>  
+                <td data-th="status" class="selectTd">${element.status}</td>
+                <td data-th="name" class="elementName selectTd">${element.protocol_name}</td>
+                <td data-th="checksum" class="${protocolChecksumStatusClass} selectTd">${element.protocol_checksum__status}</td>
+                <td data-th="ttl" class="${ttlClass} selectTd">${element.ttl}</td>            
+                <td data-th="ipFrom" class="selectTd">${ipFrom}</td>                 
+                <td data-th="ipDest" class="selectTd">${ipDest}</td>                           
             </tr>`;  
          
         });
@@ -35,6 +35,22 @@ fetch('http://localhost/J3M/ajax/getDataJson.php')
         // SI  la value d'option est égale à l'écriture qu'il y a dans protocole name ajouter class trBlock à tr
         const trame = document.querySelectorAll('.tra');
         const selectList = document.querySelector('#select-list'); 
+        const selectTd = document.querySelectorAll('.selectTd');
+        
+        console.log(selectTd);
+        console.log(selectTd.innerText);
+
+        
+        // console.log(trame);
+
+        // Ouverture de trame avec plus de données
+        trame.forEach(elements =>{
+            elements.addEventListener('click', event => {
+                console.log(selectTd.innerText);
+              });
+        })
+
+        // SELECT LIST
 
         selectList.addEventListener("change",() => {
         // protocolName = selectList.value
@@ -50,7 +66,14 @@ fetch('http://localhost/J3M/ajax/getDataJson.php')
                 element.classList.add("trNone");
             }
         });
+
+
+      
+
+
+
     })
+
 });
 
 

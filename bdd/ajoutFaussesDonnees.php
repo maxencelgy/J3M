2d6a90c1760b0c4f7e2fba789e8a9d9cbf58820d
MaxenceBossin <maxbossin25@gmail.com>
2022-01-17 09:50:19 +0100

    :fire: reset lundi matin  :fire:

---
+++ bdd/ajoutFaussesDonnees.php
@@ -0,0 +1,15 @@
+<?php
+
+
+for($i=0; $i<300 ;$i++){
+    echo'<br> boucle 1 : '.$i;
+}
+
+for($i=0; $i<500 ;$i++){
+    echo'<br> boucle 2 : '.$i;
+}
+
+for($i=0; $i<300 ;$i++){
+    echo'<br> boucle 3 : '.$i; 
+}
+?>

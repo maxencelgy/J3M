1ffc31c346ea7a7d2360be554a3c7d5f8092c28b
Pinto Jody <pintojody20@gmail.com>
2022-01-18 16:49:31 +0100

    Ajout mdp oublié :fire:

--- inc/fonction/request.php
+++ inc/fonction/request.php
@@ -57,6 +57,38 @@ function getJsonDataUdp()
     return json_encode($query->fetchAll());
 }
 
+//Requete pour recuper//Function pour recuperer user après clique sur lien du mail changement de mdp
+function getUserResetPassword($email,$token){
+    global $pdo;
+    $sql="SELECT * FROM user WHERE email = :email AND token= :token";
+    $query = $pdo->prepare($sql);
+    $query ->bindValue(':email',$email,PDO::PARAM_INT);
+    $query ->bindValue(':token',$token,PDO::PARAM_INT);
+    $query->execute();
+    return $query->fetch();
+}
+//Recupère utilisateur par son token
+function getUserByToken($token){
+    global $pdo;
+    $sql = "SELECT * FROM user WHERE token=:token ";
+    $query = $pdo->prepare($sql);
+    $query->bindValue(':token',$token,PDO::PARAM_STR);
+    $query->execute();
+    return $query->fetch();
+}
+//Changement mot de passe en bdd
+function updatePassword($password, $token){
+    global $pdo;
+    $sql = "UPDATE user SET `mdp`=:password WHERE token=:token";
+    $query = $pdo->prepare($sql);
+    $query->bindValue(':password',$password,PDO::PARAM_STR);
+    $query->bindValue(':token',$token,PDO::PARAM_STR);
+    $query->execute();
+}
+
+
+
+
 function requestVerifMailRegister($email){
     global $pdo;
     $sql = "SELECT * FROM user WHERE email = :email";

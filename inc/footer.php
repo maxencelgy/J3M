81a9a21518f5cd6adfbfcf90916279aacd5fa2a4
Pinto Jody <pintojody20@gmail.com>
2022-01-20 09:32:26 +0100

    recup

--- inc/footer.php
+++ inc/footer.php
@@ -73,7 +73,7 @@ if($page === 'index.php'){?>
     if($page === 'dashboard.php'){?><script src="../asset/js/dashboard.js"></script><?php }
     if($page === 'detail.php'){?><script  src="../asset/js/detail.js"></script><?php }
 }else {?>
-<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
    <script src="../asset/js/burger.js"></script>
     <?php } ?>
 </body>
